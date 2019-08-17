c50139b46a0fd0971afe42c986b9291c436619b1
dlebansais <dlebansais@gmail.com>
2019-08-17 16:54:34 +0200

    Misc update.

---
+++ PgBrew/Converters/PositiveIntToObjectConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Converters
+{
+    [ValueConversion(typeof(int), typeof(object))]
+    public class PositiveIntToObjectConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int IntValue = (int)value;
+            CompositeCollection CollectionOfItems = parameter as CompositeCollection;
+
+            return IntValue >= 0 ? CollectionOfItems[1] : CollectionOfItems[0];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
